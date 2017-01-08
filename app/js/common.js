327f1e663190744586741d13e10adef8850702be
Kostiantyn Koval <kostiantyn.koval@gmail.com>
2017-01-09 01:21:30 +0200

    commit 3 almost done with design

--- app/js/common.js
+++ app/js/common.js
@@ -3,23 +3,26 @@ $(function(){
 		var x = e.pageX, y = e.pageY;
 		$('header').css('background-position', (x/200)+'% '+(44+(y/200))+'%');
 	});
-	$(window).on('resize', function setDisplay(){
+	/*$(window).on('resize', function setDisplay(){
 		var headerHeight = $('header').height();
-		var orderHeight = $('.order').height();
-		var orderMarginTop = orderHeight*0.04;
-		$('.order').css('margin-top', orderMarginTop+headerHeight+'px');
-		$('section:not(.order)').each(function (){
+		var orderHeight = $('#order').height();
+		var orderMarginTop = orderHeight*0.02;
+		/*$('.order').css('margin-top', orderMarginTop+headerHeight+'px');
+		$('section').each(function (){
 			$(this).css('margin-top', orderMarginTop+'px');
 		});
-	});
+	});*/
 	$(window).ready().trigger('resize');
 	$(window).on('scroll', function (){
 		var st = $(this).scrollTop();
-		$('.forest').css('background-position', 'center '+st/5+'%');
-		$('.orderBlock').css({'transform': 'translateY('+st/2.2+'px)'});
-		$('#gallery').css('background-position', 'center '+(35+st/17)+'%');
-		console.log(35+st/17);
+		$('.nailFrench img').css({'transform': 'translateY('+st/3+'px)'});
+		$('.orderBlock').css({'transform': 'translateY('+st/2+'px)'});
+		$('.bg-gallery').css({'transform': 'translateY('+st/5+'px)'});
+		$('#prices img').css({'transform': 'translateY('+st/5+'px)'});
+		//console.log(65+st/15);
 	})
-
+	$(document).ready(function(){
+		$(".fancybox").fancybox()
+	});
 	
 });
